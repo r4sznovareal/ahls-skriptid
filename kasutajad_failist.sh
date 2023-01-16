354a94ffc4d2e405f1c0e5a1bd08db1fcae0ed92
r4sznovo <74591072+r4sznovo@users.noreply.github.com>
2023-01-16 08:50:34 +0200

    Update kasutajad_failist.sh

--- kasutajad_failist.sh
+++ kasutajad_failist.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #skript, mis lisab kasutajaid failist
-if [ $# -ne 1 ]; then
+if [ $# -ne 1 ]; then #kui ei määrata parameetreid skriptile juurde, väljastatakse n-ö kasutusjuhend
         echo "Kasutusjuhend: $0 failinimi"
 else
 	failinimi=$1
 	if [ -f $failinimi -a -r $failinimi ]; then
 		echo "Fail on korras"
-		for nimi in $(cat $failinimi)
+		for nimi in $(cat $failinimi) #loeb failist $failinimi kõik read läbi ja hiljem teeb 
 		do
 			#cat abil saab vaadata faili sisu, nüüd saab kasutajaid luua
 			./lisa_kasutaja.sh $nimi #kasutaja loomise skripti käivitamine
